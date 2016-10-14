7e34c0eb9305c6c681e800255063d6400c385cd0
erickponceleao <erickponceleao@gmail.com>
2016-10-14 16:37:27 -0300

    Changing npm modules default location

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,11 @@ echo "Current directory: " $CURRENT_DIR
 #npm config set registry http://registry.npmjs.org
 #npm install --prod
 
+while : ; do
+    echo 'Holding container...'
+    sleep 10s
+done
+
 
 echo "Starting application..."
 exec "$@"
