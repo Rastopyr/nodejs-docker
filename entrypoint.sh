7506da393c62289646509db3c37062b92c4b42e1
erickponceleao <erickponceleao@gmail.com>
2016-10-14 14:14:18 -0300

    Adding app specific npm install

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ CURRENT_DIR=$(pwd)
 
 echo "Current directory: " $CURRENT_DIR
 
-echo "Installing app dependencies..."
-npm config set registry http://registry.npmjs.org
-npm install --prod
+#echo "Installing app dependencies..."
+#npm config set registry http://registry.npmjs.org
+#npm install --prod
 
 echo "Starting application..."
 exec "$@"
