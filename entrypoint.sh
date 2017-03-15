4c3f7ea0cf138b2a52aa54bb2bb0441234be90d9
Erick Ponce <erickponceleao@gmail.com>
2017-03-15 14:35:49 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 CURRENT_DIR=$(pwd)
 
+cp package.json /install
+cd /install
+npm config set registry http://registry.npmjs.org/ \
+    && npm install --prod
+
+cd ${CURRENT_DIR}
+
 echo "Current directory: " $CURRENT_DIR
 
 #echo "Installing app dependencies..."
